f2f1742b03eb60991be1acac14efa5a775de7c7d
Robert <tera420777@gmail.com>
2023-09-09 22:33:34 +0300

    Python-2

--- main2.py
+++ main2.py
@@ -1,10 +1,12 @@
-#Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. Петя помогает Кате по математике. Он задумывает два натуральных числа 
-#X и Y (X,Y≤1000), а Катя должна их отгадать. Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. Помогите 
-#Кате отгадать задуманные Петей числа.
-
-a, b = map(int, input().split())
-res = []
-for i in range(a + b):
-    if i == (a * i - b)**0.5:
-        res.append(i)
-print(*res if len(res) == 2 else res + res)
+from random import randint
+list_1 = list(randint(1, 5) for i in range(int(input("Введите кол-во кустов: "))))
+print(list_1)
+a = int(input("Введите № куста: "))
+res = 0
+if a == 1:
+    res = list_1[0] + list_1[1] + list_1[-1]
+elif a == len(list_1):
+    res = list_1[-2] + list_1[-1] + list_1[0]
+else:
+    res = list_1[a-1] + list_1[a-2] + list_1[a]
+print(res, "ягод")
